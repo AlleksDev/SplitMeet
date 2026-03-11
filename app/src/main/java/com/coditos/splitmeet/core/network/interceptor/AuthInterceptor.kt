115bcceaf7babdb38011df150b8f7b20f1159ee7
JosephAntonyDev <joseantoniopintoaguilar@gmail.com>
2026-03-11 00:14:13 -0600

    feat: configure SSE OkHttpClient and refactor AuthInterceptor

--- app/src/main/java/com/coditos/splitmeet/core/network/interceptor/AuthInterceptor.kt
+++ app/src/main/java/com/coditos/splitmeet/core/network/interceptor/AuthInterceptor.kt
@@ -1,5 +1,7 @@
 package com.coditos.splitmeet.core.network.interceptor
 
+import com.coditos.splitmeet.core.storage.TokenDataStore
+import kotlinx.coroutines.runBlocking
 import okhttp3.Interceptor
 import okhttp3.Response
 import javax.inject.Inject
@@ -7,13 +9,17 @@ import javax.inject.Singleton
 
 @Singleton
 class AuthInterceptor @Inject constructor(
-    private val tokenProvider: () -> String
+    private val tokenDataStore: TokenDataStore
 ) : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
-        val request = chain.request().newBuilder()
-            .addHeader("Authorization", "Bearer ${tokenProvider()}")
-            .build()
+        val token = runBlocking { tokenDataStore.getToken() }
 
-        return chain.proceed(request)
+        val requestBuilder = chain.request().newBuilder()
+
+        if (!token.isNullOrEmpty()) {
+            requestBuilder.addHeader("Authorization", "Bearer $token")
+        }
+
+        return chain.proceed(requestBuilder.build())
     }
 }
--- app/src/main/java/com/coditos/splitmeet/core/network/interceptor/LoggingInterceptor.kt
+++
@@ -1,9 +0,0 @@
-package com.coditos.splitmeet.core.network.interceptor
-
-import okhttp3.logging.HttpLoggingInterceptor
-
-fun provideLoggingInterceptor(): HttpLoggingInterceptor {
-    return HttpLoggingInterceptor().apply {
-        level = HttpLoggingInterceptor.Level.BODY
-    }
-}
