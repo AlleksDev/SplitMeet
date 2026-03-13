4deaccd10aa2d7b253baedc23dc87eeca4223e5a
Alejandro <149030059+AlleksDev@users.noreply.github.com>
2026-03-13 03:41:13 -0600

    Revert "Alejandromendoza"

--- app/src/main/java/com/coditos/splitmeet/features/home/presentation/screens/HomeUiState.kt
+++ app/src/main/java/com/coditos/splitmeet/features/home/presentation/screens/HomeUiState.kt
@@ -3,8 +3,7 @@ package com.coditos.splitmeet.features.home.presentation.screens
 import com.coditos.splitmeet.features.home.domain.entities.Outing
 
 data class HomeUiState(
-    val isLoading: Boolean = true,
-    val isSyncing: Boolean = false,
+    val isLoading: Boolean = false,
     val isRefreshing: Boolean = false,
     val outings: List<Outing> = emptyList(),
     val selectedTabIndex: Int = 0,
