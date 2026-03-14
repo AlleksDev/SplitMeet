38492ad6edde34f0de37f41539eec6e165c9eac5
AlleksDev <aimr2910@gmail.com>
2026-03-13 20:19:41 -0600

    Fix: Use Cases DI in ManageOuting

---
+++ app/src/main/java/com/coditos/splitmeet/features/manageouting/domain/usecases/ManageOutingUseCases.kt
@@ -0,0 +1,6 @@
+package com.coditos.splitmeet.features.manageOuting.domain.usecases
+
+data class ManageOutingUseCases(
+    val createOuting: CreateOutingUseCase,
+    val getCategories: GetCategoriesUseCase
+)
